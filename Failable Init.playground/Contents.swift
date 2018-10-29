c0d7e52a951c4946ff91f07406b6261787c644bd
Sagar <sagarsandy492@gmail.com>
2018-10-29 20:13:02 +0530

    Failable init

--- Failable Init.playground/Contents.swift
+++ Failable Init.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var str = "Hello, playground"
+
+
+// Failable init
+// Initilaization of an object may fail sometimes..
+
+
+class Course {
+    
+    let name : String
+    
+    init?(name: String) {
+        
+        if name.isEmpty {
+            return nil
+        }
+        
+        self.name = name
+    }
+}
+
+let objC = Course(name: "")   // If we don't pass empty value, the initialization will fail.
+
+// You have to unwrap the object here, because it is an optional object
+//objC!.name  // Force unwrapping  --> gives error becuase of empty object.
+
+if let newObjC = objC {
+    print(newObjC.name)
+} else {
+    print("No object found...")
+}
+
+
